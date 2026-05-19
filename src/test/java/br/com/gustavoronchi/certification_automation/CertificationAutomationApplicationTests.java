e4ef958651ebcaad125376cab78cd2fe87c9ef57
Gustavo Caçador <gustavoirl456@outlook.com>
2026-05-18 22:33:46 -0300

    initial commit

---
+++ src/test/java/br/com/gustavoronchi/certification_automation/CertificationAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoronchi.certification_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
